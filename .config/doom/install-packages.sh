0f641a8956e903933b763cc7d22278b55619b795
Moritz Gleissner <moritz@gleissner.de>
2023-03-18 16:03:07 +0100

    nothing to see here

---
+++ .config/doom/install-packages.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yay -Syu --needed libvterm languagetool nodejs npm yarn lldb gdb unzip docker docker-compose docker-machine editorconfig-checker sqlite ripgrep git wl-clipboard clang ccls go gopls jdk11-openjdk texlive-bin texlive-core texlive-latexextra texlive-science gnuplot marked python pyright rubocop rustup mpd mpc maim scrot gnome-screenshot rust-analyzer tidy stylelint python-pipenv curl gcc make ncurses man-pages xdg-utils nodejs-dockerfile-language-server wordnet-cli cmake-language-server proselint ktlint shellcheck-bin js-beautify
