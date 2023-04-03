635f5a0ec8023193a1f553a355c26e9d1d7fad26
Moritz Gleissner <moritz@gleissner.de>
2023-04-03 15:56:43 +0200

    fixed icons and emacs spell

--- .config/doom/install-packages.sh
+++ .config/doom/install-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yay -Syu --noconfirm --needed emacs-nativecomp libvterm languagetool nodejs npm yarn lldb gdb unzip docker docker-compose docker-machine editorconfig-checker sqlite ripgrep git wl-clipboard clang ccls go gopls jdk11-openjdk texlive-most gnuplot marked python pyright rubocop rustup mpd mpc maim scrot gnome-screenshot rust-analyzer tidy stylelint python-pipenv curl gcc make ncurses man-pages xdg-utils nodejs-dockerfile-language-server wordnet-cli cmake-language-server proselint ktlint shellcheck-bin js-beautify ttf-jetbrains-mono
+yay -Syu --noconfirm --needed emacs-nativecomp hunspell hunspell-en_us libvterm languagetool nodejs npm yarn lldb gdb unzip docker docker-compose docker-machine editorconfig-checker sqlite ripgrep git wl-clipboard clang ccls go gopls jdk11-openjdk texlive-most gnuplot marked python pyright rubocop rustup mpd mpc maim scrot gnome-screenshot rust-analyzer tidy stylelint python-pipenv curl gcc make ncurses man-pages xdg-utils nodejs-dockerfile-language-server wordnet-cli cmake-language-server proselint ktlint shellcheck-bin js-beautify ttf-jetbrains-mono
 
 # pip installs
 pip install pytest nose black pyflakes isort python-language-server grib
