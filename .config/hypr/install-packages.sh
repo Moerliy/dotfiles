0cb3984564a5b02a3584a9da238eead5f8a32d9e
Moritz Gleissner <moritz@gleissner.de>
2023-05-03 15:28:41 +0200

    commented omf install out

--- .config/hypr/install-packages.sh
+++ .config/hypr/install-packages.sh
@@ -2,6 +2,6 @@
 yay -Syu --noconfirm --needed hyprland-legacyrenderer-git mako pipewire wireplumber alsa-utils pipewire-alsa pipewire-pulse bluez bluez-utils xdg-desktop-portal-hyprland-git polkit-kde-agent qt5-wayland qt6-wayland sddm-catppuccin-git waybar-hyprland-git alacritty kitty qt5ct lxappearance catppuccin-gtk-theme-mocha firefox armcord-bin youtubemusic-nativefier nextcloud-client nautilus electron20-bin wlogout swayidle swaylock-effects wofi pamixer playerctl slurp grim jq swaybg wf-recorder wl-clipboard copyq wlroots light xorg-xwayland yad viewnior mpv bemenu-wayland hyprpicker-git fish starship neofetch btop htop vim neovim exa ttf-nerd-fonts-symbols-2048-em ttf-hack ttf-ubuntu-font-family ttf-kanjistrokeorders ttf-iosevka adobe-source-han-mono-jp-fonts noto-fonts-emoji packer breeze bat-extras
 
 # oh my fish instalation
-if [ ! -d "$HOME/.local/share/omf" ]; then
-    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-fi
+# if [ ! -d "$HOME/.local/share/omf" ]; then
+#     curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+# fi
