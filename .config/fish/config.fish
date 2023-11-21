a01376272061813bb4324dd045dc13734b522421
Moritz Gleissner <moritz@gleisser.de>
2023-11-21 01:34:19 +0100

    fish fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,18 +12,18 @@ fish_add_path --path "$HOME/.ghcup/bin"
 starship init fish | source
 
 # oh my fish settings
-set -g theme_display_git yes
-set -g theme_display_git_untracked yes
-set -g theme_display_git_ahead_verbose yes
-set -g theme_display_hg yes
-set -g theme_display_virtualenv no
-set -g theme_display_ruby yes
-set -g theme_display_user yes
-set -g theme_title_display_process yes
-set -g theme_title_display_path no
-set -g theme_date_format "+%a %H:%M"
-set -g theme_avoid_ambiguous_glyphs yes
-set -g default_user your_normal_user
+#set -g theme_display_git yes
+#set -g theme_display_git_untracked yes
+#set -g theme_display_git_ahead_verbose yes
+#set -g theme_display_hg yes
+#set -g theme_display_virtualenv no
+#set -g theme_display_ruby yes
+#set -g theme_display_user yes
+#set -g theme_title_display_process yes
+#set -g theme_title_display_path no
+#set -g theme_date_format "+%a %H:%M"
+#set -g theme_avoid_ambiguous_glyphs yes
+#set -g default_user your_normal_user
 
 
 ### EXPORT ###
