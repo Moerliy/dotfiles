13bd4aae95e905c9e7a014ffcbf5e63d8b5fb68f
Moritz Gleissner <moritz@gleisser.de>
2023-11-26 14:48:59 +0100

    ui and tests

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -94,15 +94,16 @@ return {
   },
   {
     "b0o/incline.nvim",
+    dependencies = { "catppuccin/nvim", name = "catppuccin" },
     event = "BufReadPre",
     priority = 1200,
     config = function()
-      local colors = require("tokyonight.colors").setup()
+      local colors = require("catppuccin.palettes").get_palette("mocha")
       require("incline").setup({
         highlight = {
           groups = {
-            InclineNormal = { guibg = colors.bg_statusline, guifg = colors.fg_dark },
-            InclineNormalNC = { guibg = colors.bg_statusline, guifg = colors.fg_dark },
+            InclineNormal = { guibg = colors.surface2, guifg = colors.sapphire },
+            InclineNormalNC = { guibg = colors.surface2, guifg = colors.sapphire },
           },
         },
         window = { margin = { vertical = 0, horizontal = 1 } },
